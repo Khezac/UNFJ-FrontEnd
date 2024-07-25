360a8ed172c403f86f12c9710b48d45ee3e81a07
Khezac <khezac1@gmail.com>
2024-07-25 19:31:47 -0300

    feat: página inicial completa, favicon e titulo da pag alterados

--- UNFJ-FrontEnd/src/main.tsx
+++ UNFJ-FrontEnd/src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Home } from './pages/Home'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header/>
     <Home/>
+    <Footer/>
   </React.StrictMode>,
 )
